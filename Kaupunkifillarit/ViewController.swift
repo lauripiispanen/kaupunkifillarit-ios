a043cc011f6d61fe4b60efd8fc862f6eaca206df
Lauri Piispanen <lauri@piispanen.fi>
2016-05-04 19:43:27 +0300

    Initial Commit

---
+++ Kaupunkifillarit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaupunkifillarit
+//
+//  Created by Lauri Piispanen on 04/05/16.
+//  Copyright © 2016 Lauri Piispanen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
