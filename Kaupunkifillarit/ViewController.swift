6401fa12a3cbddfe9da4ade897a55947fb67f4b3
Lauri Piispanen <lauri@piispanen.fi>
2016-05-06 13:32:27 +0300

    get rid of extraneous controls for first version

--- Kaupunkifillarit/ViewController.swift
+++ Kaupunkifillarit/ViewController.swift
@@ -13,12 +13,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     
     var map: MKMapView?
-    let locationManager = CLLocationManager()
     let dataSource = FillariDataSource()
-    var borrowing = true
-    var borrowButton: UIButton?
-    var returnButton: UIButton?
-    var nearestStationBadge: NearestStationBadge?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,93 +29,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         self.view.addSubview(map!)
         
-        let borrowButton = UIButton()
-        self.borrowButton = borrowButton
-        borrowButton.addTarget(self, action: #selector(self.startBorrowing), forControlEvents: .TouchUpInside)
-        borrowButton.backgroundColor = UIColor.whiteColor()
-        borrowButton.translatesAutoresizingMaskIntoConstraints = false
-        borrowButton.setTitle("LAINAAN", forState: .Normal)
-        borrowButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        
-        self.view.addSubview(borrowButton)
-        
-        let returnButton = UIButton()
-        self.returnButton = returnButton
-        returnButton.addTarget(self, action: #selector(self.startReturning), forControlEvents: .TouchUpInside)
-        returnButton.backgroundColor = UIColor.grayColor()
-        
-        returnButton.translatesAutoresizingMaskIntoConstraints = false
-        returnButton.setTitle("PALAUTAN", forState: .Normal)
-        returnButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        
-        self.view.addSubview(returnButton)
-        
-        let badge = NearestStationBadge()
-        self.nearestStationBadge = badge
-        
-        self.view.addSubview(badge.view)
-        
-        badge.view.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 20.0).active = true
-        badge.view.widthAnchor.constraintEqualToConstant(100.0).active = true
-        badge.view.heightAnchor.constraintEqualToAnchor(badge.view.widthAnchor).active = true
-        badge.view.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        
         map!.translatesAutoresizingMaskIntoConstraints = false
-        map!.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor, constant: -75.0).active = true
+        map!.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor).active = true
         map!.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true
         map!.leftAnchor.constraintEqualToAnchor(view.leftAnchor).active = true
         map!.rightAnchor.constraintEqualToAnchor(view.rightAnchor).active = true
         
-        borrowButton.topAnchor.constraintEqualToAnchor(map!.bottomAnchor).active = true
-        borrowButton.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
-        borrowButton.leftAnchor.constraintEqualToAnchor(view.leftAnchor).active = true
-        
-        returnButton.topAnchor.constraintEqualToAnchor(map!.bottomAnchor).active = true
-        returnButton.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
-        returnButton.rightAnchor.constraintEqualToAnchor(view.rightAnchor).active = true
-        returnButton.leftAnchor.constraintEqualToAnchor(borrowButton.rightAnchor).active = true
-        borrowButton.widthAnchor.constraintEqualToAnchor(returnButton.widthAnchor).active = true
-        
         dataSource.startRefresh()
-        
-        self.locationManager.requestWhenInUseAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-            
-            syncLocation()
-        }
-    }
-        
-    func syncLocation() {
-        if let loc = locationManager.location {
-            locationManager(locationManager, didUpdateLocations: [loc])
-        }
-    }
-    
-    func startBorrowing() {
-        self.borrowing = true
-        self.redrawStations(dataSource.stations)
-        returnButton!.backgroundColor = UIColor.grayColor()
-        borrowButton!.backgroundColor = UIColor.whiteColor()
-        syncLocation()
-    }
-    
-    func startReturning() {
-        self.borrowing = false
-        self.redrawStations(dataSource.stations)
-        borrowButton!.backgroundColor = UIColor.grayColor()
-        returnButton!.backgroundColor = UIColor.whiteColor()
-        syncLocation()
-    }
-    
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locationManager.location,
-           let station = dataSource.nearestStation(location, borrowing: borrowing) {
-            nearestStationBadge!.setNearestStation(station, distance: distance(location)(station), count: borrowing ? station.bikesAvailable : station.spacesAvailable)
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -154,11 +69,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     func redrawStations(stations: [Station]) {
         let markers = stations.map { (station) -> StationAnnotation in
-            
-            let amount = borrowing ? station.bikesAvailable : station.spacesAvailable
             let total = station.bikesAvailable + station.spacesAvailable
             
-            let annotation = StationAnnotation(amount: amount, total: total)
+            let annotation = StationAnnotation(amount: station.bikesAvailable, total: total)
             annotation.coordinate = CLLocationCoordinate2DMake(station.lat, station.lon)
             return annotation
         }
