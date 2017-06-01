45a34214ce6b3f17a38fb77fdf975e529e7289f5
Lauri Piispanen <lauri.piispanen@reaktor.com>
2017-06-01 23:10:13 +0300

    automate screenshots

--- KaupunkifillaritUITests/KaupunkifillaritUITests.swift
+++ KaupunkifillaritUITests/KaupunkifillaritUITests.swift
@@ -18,7 +18,9 @@ class KaupunkifillaritUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,9 +30,18 @@ class KaupunkifillaritUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTakeScreenShot() {
+        let app = XCUIApplication()
+        let map = app.maps.element
+
+        Thread.sleep(forTimeInterval: 5)
+
+        snapshot("01Launch")
+
+        map.pinch(withScale: 3, velocity: 1)
+        map.swipeLeft()
+
+        snapshot("02Zoomed")
     }
     
 }
